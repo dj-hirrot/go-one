98bafd13f5fe295d6a3f19201318fdad9caa51b5
dj-hirrot <hirotoshibutani@gmail.com>
2021-09-27 18:38:13 +0900

    added: init sql

---
+++ docker/mysql/db/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE one_develop;
+use one_develop;
+
+CREATE TABLE users (
+  id int(10) unsigned not null auto_increment,
+  name varchar(255) not null,
+  created_time datetime not null default current_timestamp,
+  updated_time datetime not null default current_timestamp on update current_timestamp,
+  primary key (id)
+);
